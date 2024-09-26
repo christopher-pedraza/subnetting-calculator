5df6fb2eb5494b5c262221180a58494b2a4d14e6
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2024-09-26 07:25:31 -0600

    Agregar imports

---
+++ subnetting-calculator/src/pages/calculator/Calculator.jsx
@@ -0,0 +1,40 @@
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
+import { Button, ButtonGroup } from "@nextui-org/button";
+import { Checkbox } from "@nextui-org/checkbox";
+import { Divider } from "@nextui-org/divider";
+import { Input } from "@nextui-org/input";
+import {
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+} from "@nextui-org/modal";
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    NavbarMenuToggle,
+    NavbarMenu,
+    NavbarMenuItem,
+} from "@nextui-org/navbar";
+import { Snippet } from "@nextui-org/snippet";
+import {
+    Table,
+    TableHeader,
+    TableBody,
+    TableColumn,
+    TableRow,
+    TableCell,
+} from "@nextui-org/table";
+
+function Calculator() {
+    return (
+        <div>
+            <h1>Calculator</h1>
+        </div>
+    );
+}
+
+export default Calculator;
